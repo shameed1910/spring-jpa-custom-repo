2fb8a8faa4012529aec479270dd18b48d04e9166
shameed1910 <shameed1910@gmail.com>
2020-12-06 19:44:46 +0530

    spring boot jpa custom repo

---
+++ src/main/java/com/example/custom/springbootjpacustom/SpringbootJpaCustomApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom.springbootjpacustom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaCustomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaCustomApplication.class, args);
+	}
+
+}
